423c0fe4ea2474e8153131e959c6776c37a07e3e
Pablo <p.c.ramirez5150@gmail.com>
2019-03-17 12:28:37 +0100

    Ahora si que si vamo joder

--- main.cpp
+++ main.cpp
@@ -2,76 +2,37 @@
 #include <iostream>
 #include <fstream>
 #include <cctype>
+#include <string>
+
+#include "include/TMXParser.h"
+
+using namespace std;
 
 // #include "lib/rapidxml-1.13/rapidxml.hpp"
 
 int main()
 {
-    std::ifstream openfile("Mapa.txt"); /// fichero stream lectura mapa
+    /// objeto TMXparser
+    // TMX::Parser tmx;
+    //tmx.load( "desert.tmx" );
 
-    sf::Texture tileTexture; /// variable donde vamos a almacenar la con todos los recursos del mapa
-    sf::Sprite tiles; /// variable donde vamos a almacenar los distintos sprites
+    /// Lectura de fichero
+    streampos size; // flag
+    char * memblock; // blocke de memoria
+    ifstream file ("assets/PaPablo2.tmx", ios::in|ios::binary|ios::ate); // fstream
 
-    sf::Vector2i map[100][100]; /// matriz de tiles del mapa
-    sf::Vector2i loadCounter = sf::Vector2i(0, 0); /// contador para las posiciones ya leidas
-
-    if(openfile.eof())
+    if (file.is_open())
     {
-        std::string tileLocation; /// string para almacenar el path
-        openfile >> tileLocation; /// busca el archivo dentro del path indicado
-        tileTexture.loadFromFile(tileLocation); /// cargamos textura
-        tiles.setTexture(tileTexture); /// seteamos textura a sprite
+        size = file.tellg();
+        memblock = new char [size];
+        file.seekg(0, ios::beg);
+        file.read(memblock, size);
+        file.close();
 
-        while(!openfile.eof()) ///
-        {
-            std::string str;
-            openfile >> str;
-            char x = str[0], y = str[2]; /// los primeros digitos que nos encontramos en el fichero, saltandonos la coma
-
-            if(!isdigit(x) || !isdigit(y)) /// comprobamos que esos char sean un digito, si no no ponemos ningun sprite
-                map[loadCounter.x][loadCounter.y] = sf::Vector2i(-1, -1); /// posicion vacia o de color de fondo
-            else
-                map[loadCounter.x][loadCounter.y] = sf::Vector2i(x - '0', y - '0'); /// le restamos el valor ASCII del 0 por alguna razon neonazi
-
-            if (openfile.peek() == '\n') /// buscamos el salto de linea
-            {
-                loadCounter.x = 0; /// empezamos la nueva linea de
-                loadCounter.y++; /// bajamos una fila dentro del loader
-            }
-            else /// si no hemos llegado el final de la linea aumentamos la y del loader
-                loadCounter.y++;
-        }
-
-        loadCounter.y++; /// cuando acabamos de leer el fichero saltamos una linea en el loader
+        cout << "Archivo leido correctamente";
     }
 
-    sf::RenderWindow window(sf::VideoMode(640, 580, 32), "Cargando tilemap desde archivo [modo noob]");
+    else cout << "Unable to open de file;";
 
-    while(window.isOpen())
-    {
-        sf::Event Event;
-        while(window.pollEvent(Event))
-        {
-            switch(Event.type)
-            {
-                case sf::Event::Closed:
-                    window.close();
-                    break;
-            }
-
-            window.clear(); /// limpiamos
-
-            for(int i = 0; i < loadCounter.y; i++)
-            {
-                for(int j = 0; j < loadCounter.x; j++)
-                {
-                    tiles.setPosition(i * 32, j * 32); /// contando con que el tile ocupe 32 px en la textura
-                    tiles.setTextureRect(sf::IntRect(map[i][j].x * 32, map[i][j].y * 32, 32, 32));
-                    window.draw(tiles);
-                }
-            }
-
-            window.display(); /// dibujamos
-        }
-    }
+    return 0;
 }
