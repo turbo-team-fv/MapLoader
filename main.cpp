bf9fc744f359defdf54e36ccdce35686a04b467d
Pablo <p.c.ramirez5150@gmail.com>
2019-03-17 13:11:25 +0100

    Loader de TMX funcionando

--- main.cpp
+++ main.cpp
@@ -4,35 +4,19 @@
 #include <cctype>
 #include <string>
 
-#include "include/TMXParser.h"
+#include "TMXParser.h"
 
-using namespace std;
 
 // #include "lib/rapidxml-1.13/rapidxml.hpp"
 
 int main()
 {
-    /// objeto TMXparser
-    // TMX::Parser tmx;
-    //tmx.load( "desert.tmx" );
+    // objeto TMXparser
+    TMX::Parser tmx;
+    tmx.load("assets/PaPablo.tmx");
 
-    /// Lectura de fichero
-    streampos size; // flag
-    char * memblock; // blocke de memoria
-    ifstream file ("assets/PaPablo2.tmx", ios::in|ios::binary|ios::ate); // fstream
 
-    if (file.is_open())
-    {
-        size = file.tellg();
-        memblock = new char [size];
-        file.seekg(0, ios::beg);
-        file.read(memblock, size);
-        file.close();
-
-        cout << "Archivo leido correctamente";
-    }
-
-    else cout << "Unable to open de file;";
+    // cout << "Map Version: " << tmx.mapInfo.version << endl;
 
     return 0;
 }
